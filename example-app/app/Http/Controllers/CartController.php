70e24d6683d7c93334390c6bc2596fbc53e83efc
aurelienMarron <aurelien.marron@le-campus-numerique.fr>
2021-07-06 14:11:19 +0200

    Ajout au panier ok

--- example-app/app/Http/Controllers/CartController.php
+++ example-app/app/Http/Controllers/CartController.php
@@ -1,17 +1,35 @@
 <?php
 
-
 namespace App\Http\Controllers;
-use App\Models\Cart;
-use App\Models\Order;
 
+use Illuminate\Http\Request;
+use App\Models\Product;
 
 class CartController extends Controller
 {
+    public function cart(Request $request){
+        $session = $request->session()->get('panier') ?? [];
+        $data = [];
 
+        $i=0;
+        foreach ($session as $product_id => $quantity){
+            $data [$i]['produit'] = Product::find($product_id);
+            $data [$i]['quantity'] = $quantity;
+            $i++;
+        }
 
-    public function cart(Order $orders){
-        return view('ListeCommande',
-            ['orders'=>$orders]);
+        return view('/cart', ['data' => $data]);
+    }
+
+    public function add(Product $product, Request $request){
+        $data=[];
+        $data[$product->id]=$request->get('quantity');
+        if ($request->session()->has('panier'))
+        {
+            $data += $request->session()->get('panier');
+        }
+        $request->session()->put('panier', $data);
+
+        return redirect()->route('product_show', $product->id)->withMessage('Produit ajouté au panier');
     }
 }
